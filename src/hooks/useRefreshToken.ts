13c1bcb95ee076cc80ae51962c37175e492ce50a
VitthalGund <vitthalgund30@gmail.com>
2023-12-27 23:27:06 +0530

    useRefreshToken custom hook added for revalidation

---
+++ src/hooks/useRefreshToken.ts
@@ -0,0 +1,34 @@
+import axios from "../api/axios";
+import { useContext } from "react";
+import AuthenticationContext from "../context/Auth/userContext";
+import { AuthContext } from "../types/authContext";
+
+const useRefreshToken = () => {
+  const { auth, userData, updateAuth, updateUserData } = useContext(
+    AuthenticationContext
+  ) as AuthContext;
+
+  const refresh = async () => {
+    const response = await axios.get("/refresh", {
+      withCredentials: true,
+    });
+    updateAuth({
+      ...auth,
+      accessToken: response.data.accessToken,
+      username: response.data.username,
+      email: response.data.email,
+      roles: response.data.roles,
+    });
+
+    updateUserData({
+      ...userData,
+      username: response.data.username,
+      email: response.data.email,
+    });
+
+    return response.data.accessToken;
+  };
+  return refresh;
+};
+
+export default useRefreshToken;
