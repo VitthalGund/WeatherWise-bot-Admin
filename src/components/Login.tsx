fcc13ba144df7eddfe7452e50f6f4bf506a9827a
VitthalGund <vitthalgund30@gmail.com>
2023-12-30 15:43:25 +0530

    Google Auth integrated

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { ChangeEvent, useState, useEffect, useContext } from "react";
 import axios from "../api/axios";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import UserContext from '../context/Auth/userContext.ts';
 import { AuthContext } from "../types/authContext.ts";
+import { useGoogleLogin } from "@react-oauth/google";
 
 const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export default function LogIn() {
@@ -16,7 +17,9 @@ export default function LogIn() {
     useEffect(() => {
         localStorage.setItem("persist", JSON.stringify(persist));
     }, [persist])
-    const [localperist, setLocalPersist] = useState(persist == "true" ? true : false);
+
+    // const [localperist, setLocalPersist] = useState(persist == "true" ? true : false);
+
     const [user, setUser] = useState({
         email: "TelegramBotAdmin@bot.com",
         password: "BotAdmin@Vitthal29"
@@ -37,9 +40,9 @@ export default function LogIn() {
         setButtonDisable(!(isEmailValid && isPasswordValid));
     }, [user]);
 
-    const togglePersist = () => {
-        setLocalPersist((prev) => !prev);
-    };
+    // const togglePersist = () => {
+    //     setLocalPersist((prev) => !prev);
+    // };
 
     const handleClickShowPassword = () => {
 
@@ -81,6 +84,22 @@ export default function LogIn() {
             setButtonDisable(false);
         }
     };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async function handleGoogleLoginSuccess(tokenResponse: any) {
+        const accessToken = tokenResponse.access_token;
+        const credentails = await axios.post("/auth/google", { googleAccessToken: accessToken });
+
+        updateAuth({
+            email: credentails.data.email,
+            roles: credentails.data.roles,
+            accessToken: credentails.data.accessToken
+        });
+        toast.success("Login Successfully!")
+        router("/")
+    }
+    const login = useGoogleLogin({ onSuccess: handleGoogleLoginSuccess });
+
     return (
         <>
             <div className="flex flex-col items-center justify-center max-h-screen py-2">
@@ -136,7 +155,7 @@ export default function LogIn() {
                                             </svg>}
                                     </div>
                                 </div>
-                                <div className="flex flex-row-reverse justify-between">
+                                {/* <div className="flex flex-row-reverse justify-between">
                                     <label htmlFor="showPassword" className="mb-2">
                                         <input
                                             type="checkbox"
@@ -147,12 +166,15 @@ export default function LogIn() {
                                             className='mr-1'
                                         /> Remember Me</label>
                                     <Link to={"/forgot"} className="text-sm text-blue-600 hover:text-blue-700 pb-2">Forgot Password?</Link>
-                                </div>
+                                </div> */}
                                 <button className="bg-blue-500 hover:bg-blue-700 rounded-xl text-white py-2 hover:scale-105 duration-300 disabled:cursor-not-allowed"
                                     onClick={onLogin} disabled={buttonDisable}>Login</button>
                             </form>
 
-                            <button className="bg-white border-0 py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300">
+                            <button className="bg-white border-0 py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300"
+                                onClick={
+                                    () => login()}
+                            >
                                 <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
                                     <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
                                     <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
@@ -168,14 +190,14 @@ export default function LogIn() {
                                 </p>
                             </div> */}
 
-                            <div className="text-sm flex items-center justify-center mt-3">
+                            {/* <div className="text-sm flex items-center justify-center mt-3">
                                 <p>Don&apos;t have an account?</p>
                                 <Link
                                     className="text-blue-600 px-1 text-base"
                                     // className="bg-blue border-0 py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300"
                                     to={"/signup"}
                                 >Register</Link>
-                            </div>
+                            </div> */}
                         </div>
 
                         {/* <!--       Image --> */}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import SetData from "./context/Auth/SetData.jsx"
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SetData>
-      <App />
+      <GoogleOAuthProvider
+        clientId={"735548005010-fdok85icp3ehcav3usp99d96sg1hovuk.apps.googleusercontent.com"}>
+        <App />
+      </GoogleOAuthProvider>
     </SetData>
   </React.StrictMode>,
 )
